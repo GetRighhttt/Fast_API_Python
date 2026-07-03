26dff6068a61a303d26662b4b3442d4d572ac823
Stefan Bayne <stefanbayne@gmail.com>
2026-07-03 09:02:57 -0500

    testing campaign and mock data

--- main.py
+++ main.py
@@ -1,28 +1,95 @@
-from fastapi import FastAPI
-from datetime import date, datetime
+from datetime import datetime
+from http.client import HTTPException
+from typing import Any
+
+from fastapi import FastAPI, HTTPException
 
 # how we define the app
-app = FastAPI()
+app = FastAPI(root_path="/api/v1")
+
 
 # declaration of the root endpoint
 @app.get("/")
 async def root():
-    return{"message": "Hello World"}
+    return {"message": "Root endpoint of the API"}
+
+
+campaigns_list: Any = [
+    {
+        "campaign_id": 1,
+        "name": "Emerald Coast",
+        "due_date": datetime.now(),
+        "created_at": datetime.now(),
+    },
+    {
+        "campaign_id": 2,
+        "name": "Be the Magic",
+        "due_date": datetime.now(),
+        "created_at": datetime.now(),
+    },
+    {
+        "campaign_id": 3,
+        "name": "Wizard of Emerald",
+        "due_date": datetime.now(),
+        "created_at": datetime.now(),
+    },
+    {
+        "campaign_id": 4,
+        "name": "Oz of City",
+        "due_date": datetime.now(),
+        "created_at": datetime.now(),
+    },
+]
+
+person: Any = [
+    {
+        "first_name": "stefan",
+        "last_name": "Bayne",
+    },
+    {
+        "first_name": "vincentjr",
+        "last_name": "bayne",
+    },
+    {
+        "first_name": "shawn",
+        "last_name": "bayne",
+    },
+    {
+        "first_name": "vincentsr",
+        "last_name": "bayne",
+    }
+]
+
 
 @app.get("/campaigns")
-async def campaigns(
-    campaign_id: int,
-    name: str,
-    due_date: date,
-    created_at: datetime,
-    updated_at: datetime,
-    status: str,
-):
-    return {
-        "campaign_id": campaign_id,
-        "name": name,
-        "due_date": due_date,
-        "created_at": created_at,
-        "updated_at": updated_at,
-        "status": status,
-    }
+async def read_campaigns():
+    return {"campaigns": campaigns_list}
+
+
+@app.get("/campaigns/{c_id}")
+async def read_campaign_by_id(c_id: int):
+    for each_campaign in campaigns_list:
+        if each_campaign["campaign_id"] == c_id:
+            return {"campaigns": each_campaign}
+    return HTTPException(status_code=404, detail="Campaign not found")
+
+
+@app.post("/campaigns")
+async def create_campaign(campaign: dict):
+    campaigns_list.append(campaign)
+    return {"campaigns": campaigns_list}
+
+
+@app.get("/names")
+async def read_names():
+    return {"names": person}
+
+
+@app.get("/names/{person_name}")
+async def read_person_first_name(person_name: str):
+    for each_name in person:
+        if each_name["first_name"] == person_name:
+            return {"first_name": each_name}
+        elif each_name["last_name"] == person_name:
+            return {"last_name": each_name}
+    raise HTTPException(status_code=404, detail="Last name not found")
